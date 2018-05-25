f7a6d58106dd3bbd010feda67040cea773e547ec
Paul Varache <paul@kano.me>
2018-05-25 11:24:02 +0100

    Module

---
+++ modulizer.sh
@@ -0,0 +1,7 @@
+modulizer --out . \
+--import-style name \
+--npm-name @kano/kwc-auth \
+--npm-version 3.0.0-beta.1 \
+--dependency-mapping kwc-icons,@kano/kwc-icons,3.0.0-beta.1 \
+--dependency-mapping kwc-style,@kano/kwc-style,3.0.0-beta.1 \
+--dependency-mapping kwc-behaviours,@kano/kwc-behaviors,3.0.0-beta.1
