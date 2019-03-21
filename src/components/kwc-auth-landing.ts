f64d21cec6b31731544b5a3a3a98e3be4111b6ce
natasharamburrun <r_natasha100@hotmail.com>
2019-03-21 15:19:37 +0000

    linter changes

--- src/components/kwc-auth-landing.ts
+++ src/components/kwc-auth-landing.ts
@@ -31,7 +31,6 @@ export class LandingPage extends LitElement {
     constructor() {
         super();
         this.view = '';
-
     }
     
     render() {
--- src/styles.ts
+++ src/styles.ts
@@ -11,23 +11,19 @@ export const styles = css`
     padding: 12px;
     border-radius: 12px;
 }
-
 h2 {
     color: #414a51;
     font-size: 24px;
     text-align: left;
     margin-bottom: 32px;
 }
-
 h4 {
     text-align: center;
 }
-
 button {
     font-family: var(--font-body);
     justify-content: center;
 }
-
 input {
     display: block;
     border: 1px solid #ccc;
@@ -47,12 +43,10 @@ input[type="password"] {
     height: 32px;
     color: #414A51;
 }
-
 .a {
     color: #FF6900;
     text-decoration: underline;
 }
-
 .link-wrapper {
     display: flex;
     flex-flow: column;
